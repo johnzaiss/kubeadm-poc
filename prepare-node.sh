4790dbe90f74f7ec61a3ad3c5fb7192d13a4fdba
John Zaiss <john.zaiss@greenious.io>
2020-10-08 14:16:21 -0500

    sample manifest

--- prepare-node.sh
+++ prepare-node.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 K8S_VER="1.16.15"
 archive="k8s_"$K8S_VER"_offline"
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+
+sysctl --system
+
 #
 # ------ Kubeadm Binaries ------
 #
